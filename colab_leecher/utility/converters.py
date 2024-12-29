14e607269d97a560dd56d138832c2458236957d3
nekaru <32153003+ninajika@users.noreply.github.com>
2024-12-29 14:32:54 +0700

    feat: treat "abcd 16 84.mp4" as normal

--- colab_leecher/utility/converters.py
+++ colab_leecher/utility/converters.py
@@ -302,7 +302,7 @@ async def splitVideo(file_path, max_size, remove: bool):
     just_name, extension = ospath.splitext(filename)
 
     # FFmpeg command to get video information in JSON format
-    cmd = ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", file_path]
+    cmd = ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", f"\"{file_path}\""]
 
     bitrate = None
     try:
@@ -321,7 +321,7 @@ async def splitVideo(file_path, max_size, remove: bool):
     # Calculate duration in seconds
     duration = int(target_size_bits / bitrate)
 
-    cmd = f'ffmpeg -i {file_path} -c copy -f segment -segment_time {duration} -reset_timestamps 1 "{Paths.temp_zpath}/{just_name}.part%03d{extension}"'
+    cmd = f'ffmpeg -i "{file_path}" -c copy -f segment -segment_time {duration} -reset_timestamps 1 "{Paths.temp_zpath}/{just_name}.part%03d{extension}"'
 
     Messages.status_head = f"<b>✂️ SPLITTING » </b>\n\n<code>{filename}</code>\n"
     BotTimes.task_start = datetime.now()
